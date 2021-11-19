1f7cf58502ae69416971a67da51e3f7722510856
lindanyonje <lindaatieno24@gmail.com>
2021-11-19 15:18:11 +0300

    Stuff

--- ROM/models.py
+++ ROM/models.py
@@ -1,26 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Customer(models.Model):
-    name=models.CharField(max_length=100, null=False, blank=False)
+class Customer(User):
     phone_number=models.IntegerField(null= True, blank= True)
-    email=models.CharField(max_length=100, null=False, blank=False)
-    password=models.CharField(max_length=100, null=False, blank=False)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at= models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.name
 
 
-class CustomerAddress(models.Model):
+class CustomerAddress(User):
     customer_id=models.ForeignKey('Customer',on_delete=models.CASCADE,blank=True,null=True)
     address=models.TextField(null=False, blank=False)
     pin=models.CharField(max_length=100, null=True, blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
-
 
 
 
@@ -44,6 +39,19 @@ class Review(models.Model):
     updated_at= models.DateTimeField(auto_now_add=True) 
 
 
+class Order(models.Model):
+    total=models.IntegerField(null= False, blank= True)
+    status=models.CharField(max_length=100, null=False, blank=False, default="Pending")
+    order_number=models.CharField(max_length=100, null=False, blank=False)
+    customer_id=models.ForeignKey('customer',on_delete=models.CASCADE,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_number
+
+
+
 class GiftCard(models.Model):
     service_id= models.ForeignKey('service',on_delete=models.CASCADE,blank=True,null=True)  
     giftcard_amount=models.IntegerField(null=False, blank=False) 
@@ -54,7 +62,7 @@ class GiftCard(models.Model):
 
 
 class Payment(models.Model):
-    service_id= models.ForeignKey('service',on_delete=models.CASCADE,blank=True,null=True)
+    order_id= models.ForeignKey('order',on_delete=models.CASCADE,blank=True,null=True)
     amount=models.IntegerField(null= True, blank= True)
     description=models.TextField(null=True, blank=True)
     invoice_number=models.CharField(max_length=100, null=False, blank=True)
