55e5940501449f980a71bde13592a7917481d914
lindanyonje <lindaatieno24@gmail.com>
2021-11-25 11:31:56 +0300

    added dashboard

--- ROM/views.py
+++ ROM/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+
+   pass
+
+
+def adminDashboard(request):
+
+    ##Declaring a dictionary used to package the data we shall
+    ##send to the frontend html template for display.
+
+   pass
