5755fceacb8b521385bf91d960806ff2cbe37406
lindanyonje <lindaatieno24@gmail.com>
2021-12-10 11:56:26 +0300

    nav sidebar

--- ROM/filters.py
+++ ROM/filters.py
@@ -5,6 +5,9 @@ from django_filters import DateFilter
 from .models import*
 
 class OrderFilter(django_filters.FilterSet):
+
+    start_date=DateFilter(field_name='created_at', lookup_expr='gte')
+    end_date=DateFilter(field_name='created_at', lookup_expr='lte')
     class Meta:
         model= Order
         fields= '__all__'
